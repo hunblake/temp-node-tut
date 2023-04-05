7397adffe58c755edd508a09f93627cef8d83f4f
Dorner Balázs <balazs.dorner@gmail.com>
2023-04-05 10:23:33 +0200

    first commit

---
+++ 1-setintevall.js
@@ -0,0 +1,15 @@
+const amount = 10
+
+if (amount < 12) {
+    console.log("small number")
+} else {
+    console.log('large number')
+}
+
+console.log('hey its my first node app!@!!!')
+
+console.log(__dirname)
+
+setInterval(() => {
+    console.log('Hello te phassszzz')
+}, 1000)
